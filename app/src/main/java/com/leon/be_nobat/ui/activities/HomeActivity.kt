ee715d8b4d84af3afef9ceb859732c507c019290
Ali Rostami <34213546+iamAliRostami@users.noreply.github.com>
2026-06-13 12:17:23 +0330

    init architecture.

--- app/src/main/java/com/leon/be_nobat/ui/activities/HomeActivity.kt
+++ app/src/main/java/com/leon/be_nobat/ui/activities/HomeActivity.kt
@@ -6,17 +6,6 @@ import com.leon.be_nobat.helpers.BaseActivity
 class HomeActivity : BaseActivity() {
     override val layoutResourceId: Int = R.layout.activity_home
 
-    /*override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }*/
-
     override fun setupViews() {
         setToolbarTitle("لیست سفارشات")
     }
